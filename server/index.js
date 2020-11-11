4325cac1d7c63458a46f5a4bc4a7c75f346027da
taitd153 <trinhdinhtai1996@gmail.com>
2020-11-11 18:00:48 +0700

    update 0:36:30

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 // for parsing application/json
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get("./", (req, res) => {
-  res.send("THIS WORKS!");
-});
+// http://localhost:5000/posts
+router.get("/", getPosts);
+router.post("/", createPost);
+
+export default router;
